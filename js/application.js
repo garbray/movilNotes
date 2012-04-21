e021c0112e063277d4cc48c9ea318d80e821bc37
Bryan Garzón Bahamón <darks@dksource.com.ar>
2012-04-21 16:46:44 -0500

    updating images css and added new functionability for backbone on application.js

---
+++ js/application.js
@@ -0,0 +1,69 @@
+var NotesApp = (function() {
+
+  var App = {
+    stores:{},
+    views:{}
+  }
+  App.stores.notes = new Store('notes');
+
+  //note Model
+  var Note = Backbone.Model.extend({
+    localStorage : App.stores.notes,
+    initialize: function(){
+      if(!this.get('title')){
+        this.set({title: "Note @"+Date()})
+      };
+      if(!this.get('body')){
+        this.set({body: "No content"})
+      };
+    }
+  });
+
+  //views
+  var newFormView = Backbone.View.extend({
+    events: {
+      "submit":"createNote"
+    },
+    createNote: function(e){
+      var attr = this.getAttributes(),
+          note = new Note();
+          console.log(note);
+
+
+      note.set(attr);
+      note.save();
+
+      //stop browser for actually submiting the form
+      e.preventDefault();
+      //stop jquery mobile to doing this form from magic
+      e.stopPropagation();
+
+      //close
+
+      $('.ui-dialog').dialog('close');
+      this.reset();
+    },
+
+    getAttributes: function(){
+      return {
+        title: this.$('form [name=title]').val(),
+        body: this.$('form [name=body]').val()
+      }
+    },
+    reset: function(){
+      //reset the form any input any textarea will be reset 
+      this.$('input, textarea').val('');
+    }
+  });
+
+
+  window.Note = Note;
+
+  $(document).ready(function(){
+    App.views.new_form = new newFormView({
+      el: $('#new')
+    });
+  })
+  
+  return App;
+})()
